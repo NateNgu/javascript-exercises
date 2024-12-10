d933f5ec9c1f005a5d4abc73fdbc101960c62c4b
NateNgu <189771634+NateNgu@users.noreply.github.com>
2024-12-09 20:25:25 -0600

    feat: add solutions for four and five
    *note: solution for five is slightly not optimal

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+
+    let newArray = []
+
+    array.forEach(item => {
+        if (!num.includes(item)) { // Put each element into the array unless an item is located in the arguments
+            newArray.push(item)
+        }
+
+    })
+
+    return newArray
+
 };
 
 // Do not edit below this line
